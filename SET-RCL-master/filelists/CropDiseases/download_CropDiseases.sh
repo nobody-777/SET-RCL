affa6899fee06e5db769907a7090924e62bc17a3
nobody-777 <77709483+nobody-777@users.noreply.github.com>
2022-04-15 12:38:27 +0800

    Delete SET-RCL-master

--- SET-RCL-master/filelists/CropDiseases/download_CropDiseases.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-unzip CropDiseases.zip
-rm -r dataset/dataset
-rm CropDiseases.zip
-
-python write_CropDiseases_filelist.py
--- SET-RCL-master/filelists/EuroSAT/download_EuroSAT.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-unzip EuroSAT.zip
-rm EuroSAT.zip
-
-python write_CropDiseases_filelist.py
